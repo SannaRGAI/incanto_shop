c9e19431eef1f3062ac977534c31c5d25892c94f
SannaRGAI <saltanat9000@gmail.com>
2023-05-28 21:53:55 +0600

    logger

--- config/settings.py
+++ config/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'drf_api_logger.middleware.api_logger_middleware.APILoggerMiddleware'
 ]
 
 ROOT_URLCONF = 'config.urls'
