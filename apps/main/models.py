ab7369a3063f51accd2000c8d2414080e8f7b7dc
SannaRGAI <saltanat9000@gmail.com>
2023-06-02 18:30:29 +0600

    migr

--- apps/main/models.py
+++ apps/main/models.py
@@ -23,7 +23,7 @@ class Product(models.Model):
     color = models.CharField(max_length=55)
     size = models.CharField(max_length=55)
     quantity = models.IntegerField()
-    image = models.ImageField(upload_to='products')
+    image = models.ImageField(upload_to='products', default='', blank=True)
     
     @property
     def average_rating(self):
