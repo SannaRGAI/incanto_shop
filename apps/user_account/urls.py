567c77c2c5016a03d3a39fd8b8ba7274f30ab406
SannaRGAI <saltanat9000@gmail.com>
2023-06-04 16:57:18 +0600

    fix forgot password url

--- apps/user_account/urls.py
+++ apps/user_account/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('activate/<str:activation_code>/', activate),
     path('change_password/', ChangePasswordView.as_view()),
     path('forgot_password/', ForgotPasswordView.as_view()),
-    path('password_confirm/<str:activation_code>/', NewPasswordView.as_view()),
+    path('forgot_password/<str:activation_code>/', NewPasswordView.as_view()),
     path('login/', TokenObtainPairView.as_view()),
     path('logout/', LogoutAPIView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()),
